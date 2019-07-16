cc978aae93f97505c3123aabee5d9fe7e9fb8569
ram kishor <ramkishor0509@gmail.com>
2019-07-16 20:40:18 +0530

    ok

--- src/main/java/org/brijframework/util/reflect/ClassUtil.java
+++ src/main/java/org/brijframework/util/reflect/ClassUtil.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -18,6 +19,38 @@ import org.brijframework.util.asserts.Assertion;
 import org.brijframework.util.validator.ValidationUtil;
 
 public abstract class ClassUtil {
+	
+	static public ParameterizedType getParameterizedType(Class<?> target) {
+	    Type[] types = getGenericType(target);
+	    if (types.length > 0 && types[0] instanceof ParameterizedType) {
+	      return (ParameterizedType) types[0];
+	    }
+	    return null;
+	  }
+
+	  static public Type[] getParameterizedTypes(Class<?> target) {
+	    Type[] types = getGenericType(target);
+	    if (types.length > 0 && types[0] instanceof ParameterizedType) {
+	      return ((ParameterizedType) types[0]).getActualTypeArguments();
+	    }
+	    return null;
+	  }
+
+	  static public Type[] getGenericType(Class<?> target) {
+	    if (target == null)
+	      return new Type[0];
+	    Type[] types = target.getGenericInterfaces();
+	    if (types.length > 0) {
+	      return types;
+	    }
+	    Type type = target.getGenericSuperclass();
+	    if (type != null) {
+	      if (type instanceof ParameterizedType) {
+	        return new Type[] { type };
+	      }
+	    }
+	    return new Type[0];
+	  }
 
 	public ClassUtil() {
 	}
